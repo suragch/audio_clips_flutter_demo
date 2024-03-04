b324078ef73f85704b800e6d8d4076983ac1f3cc
suragch <suragch@suragch.dev>
2024-03-04 08:50:34 +0800

    update for flutter 3.19

--- android/app/src/main/java/dev/suragch/audio_clips_flutter_demo/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package dev.suragch.audio_clips_flutter_demo;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const AudioApp());
 
-class MyApp extends StatefulWidget {
+class AudioApp extends StatefulWidget {
+  const AudioApp({Key? key}) : super(key: key);
+
   @override
-  _MyAppState createState() => _MyAppState();
+  State<AudioApp> createState() => _AudioAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _AudioAppState extends State<AudioApp> {
   late AudioPlayer player;
   late AudioPlayer player2;
 
@@ -36,24 +38,24 @@ class _MyAppState extends State<MyApp> {
             children: [
               ElevatedButton(
                 onPressed: () async {
-                  await player.setAsset('assets/audio/cow.mp3');
-                  // final url =
-                  //     'https://www.applesaucekids.com/sound%20effects/moo.mp3';
-                  // await player.setUrl(url);
+                  const url =
+                      'https://www.applesaucekids.com/sound%20effects/moo.mp3';
+                  await player.setUrl(url);
+                  //await player.setAsset('assets/audio/cow.mp3');
                   player.play();
                 },
-                child: Text('Cow'),
+                child: const Text('Cow'),
               ),
-              SizedBox(width: 10),
+              const SizedBox(width: 10),
               ElevatedButton(
                 onPressed: () async {
-                  await player2.setAsset('assets/audio/horse.mp3');
-                  // final url =
-                  //     'https://www.applesaucekids.com/sound%20effects/horse_whinney_2.mp3';
-                  // await player2.setUrl(url);
+                  const url =
+                      'https://www.applesaucekids.com/sound%20effects/horse_whinney_2.mp3';
+                  await player2.setUrl(url);
+                  // await player2.setAsset('assets/audio/horse.mp3');
                   player2.play();
                 },
-                child: Text('Horse'),
+                child: const Text('Horse'),
               ),
             ],
           ),
@@ -62,3 +64,68 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:just_audio/just_audio.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   late AudioPlayer player;
+//   late AudioPlayer player2;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     player = AudioPlayer();
+//     player2 = AudioPlayer();
+//   }
+
+//   @override
+//   void dispose() {
+//     player.dispose();
+//     player2.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               ElevatedButton(
+//                 onPressed: () async {
+//                   await player.setAsset('assets/audio/cow.mp3');
+//                   // final url =
+//                   //     'https://www.applesaucekids.com/sound%20effects/moo.mp3';
+//                   // await player.setUrl(url);
+//                   player.play();
+//                 },
+//                 child: Text('Cow'),
+//               ),
+//               SizedBox(width: 10),
+//               ElevatedButton(
+//                 onPressed: () async {
+//                   await player2.setAsset('assets/audio/horse.mp3');
+//                   // final url =
+//                   //     'https://www.applesaucekids.com/sound%20effects/horse_whinney_2.mp3';
+//                   // await player2.setUrl(url);
+//                   player2.play();
+//                 },
+//                 child: Text('Horse'),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
